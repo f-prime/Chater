891ff2cce936e5854fc6bdbeb48fff5862f2149b
Frankie <max00355@gmail.com>
2014-08-26 18:35:29 -0400

    Trying to fix the bug

--- client.py
+++ client.py
@@ -5,16 +5,18 @@ import socket
 
 class Client:
     def __init__(self):
+        self.frames = []
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         pygame.mixer.init()
         self.sound = pygame.mixer.Sound
         p = pyaudio.PyAudio()
-        self.pstream = p.open(format=pyaudio.paInt16, channels=1, rate=44100, input=True, frames_per_buffer=1024)
+        self.pstream = p.open(format=pyaudio.paInt16, channels=2, rate=44100, input=True, frames_per_buffer=1024)
         self.connect = ("100.1.73.128", 5555)
 
     def main(self):
         self.sock.connect(self.connect)
         thread.start_new_thread(self.talk, ())
+        thread.start_new_thread(self.stream, ())
         while True:
             data = self.sock.recv(1024)
             if data:
@@ -22,7 +24,12 @@ class Client:
 
     def talk(self):
         while True:
-            self.sock.send(self.pstream.read(1024))
+            self.frames.append(self.pstream.read(1024))
+
+    def stream(self):
+        while True:
+            if len(self.frames) > 0:
+                self.sock.send(self.frames.pop(0))
 
 
 if __name__ == "__main__":
--- server.py
+++ server.py
@@ -1,25 +1,33 @@
 import socket
+import thread
 
 class Server:
     def __init__(self):
+        self.frames = []
         self.online = []
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.host = ("0.0.0.0", 5555)
     def main(self):
         self.sock.bind(self.host)
+        thread.start_new_thread(self.send, ())
         while True:
             data, obj = self.sock.recvfrom(1024)
             print obj
             if obj not in self.online:
                 self.online.append(obj)
 
-            self.send(data, obj)
+            self.frames.append((data, obj))
 
 
-    def send(self, data, obj):
-        for x in self.online:
-            if x != obj:
-                self.sock.sendto(data, obj)
+    def send(self):
+        while True:
+            if len(self.frames) > 0:
+                on = self.frames.pop(0)
+                data = on[0]
+                obj = on[1]
+                for x in self.online:
+                    if x != obj:
+                        self.sock.sendto(data, obj)
 
 
 if __name__ == "__main__":
